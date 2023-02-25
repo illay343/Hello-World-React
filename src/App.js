50e2f12cd552498153a7112402a12ed4bad4aafe
illay343 <illay343.mlg@gmail.com>
2023-02-25 11:56:33 +0200

    Hello-World-React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+ <body className='hellow'>
+  <h1>Hello World</h1>
+ </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
